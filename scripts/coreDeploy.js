cfaf306b998cb6027426caf8007552557d202cd6
iotadex <wangyi_j@163.com>
2023-05-29 13:32:09 +0800

    init

---
+++ scripts/coreDeploy.js
@@ -0,0 +1,23 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const UniswapV3Factory = await hre.ethers.getContractFactory("UniswapV3Factory");
+  const uv3 = await UniswapV3Factory.deploy();
+
+  await uv3.deployed();
+
+  console.log(`Deployed UniswapV3Factory to ${uv3.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
